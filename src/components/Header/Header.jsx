beb3f876bbc8eab300c20a802a2669a6833aeef1
Software Engineering Student <briksaliha@gmail.com>
2024-05-26 03:59:22 +0200

    try to make it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Header.css'
+import {BiMenuAltRight} from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler'
 const Header = () => {
+  const [menuOpened, setMenuOpened] = React.useState(false)
+
+  const getMenuStyles = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) 
+      {
+      return { right: !menuOpened && "-100%" };
+    }
+  }
   return (
     <section className="h-wrapper">
       <div className="h-container flexCenter paddings innerWidth  ">
         <img src='./logo.png' alt="logo" width={100} />
 
-        <div className='flexCenter h-menu'>
+        <div className='flexCenter h-menu' style={getMenuStyle(menuOpened)}>
             <a href=''>Residencies</a>
             <a href=''>Our Value</a>
             <a href=''>Contact Us</a>
@@ -17,8 +27,12 @@ const Header = () => {
             
 
         </div>
+        <div className='menu-icon' onClick={() => setMenuOpened((prev) => !prev)}>
+        <BiMenuAltRight size={30} />
 
       </div>
+      </div>
+      
     </section>
   )
 }
