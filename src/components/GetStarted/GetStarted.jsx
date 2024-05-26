a9e31456b5d5f6c6c678415c35581ae03dab6c4a
Software Engineering Student <briksaliha@gmail.com>
2024-05-26 03:02:16 +0200

    create GetStarted components

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './GetStarted.css'
+const GetStarted = () => {
+  return (
+    <section  className='g-wrapper'>
+        <div className='paddings innerWidth  g-container'>
+            <div className='flexColCenter inner-container'>
+                <span className='primaryText'>Get Started with NFTs</span>
+                 <span className='secondaryText'>
+                    Subscribe to our newsletter and get notified on upcoming drops.
+                    <br/>
+                    Find your favorite NFTs in our marketplace.
+                 </span>
+                 <button className='button'>
+                    <a href='briksaliha@gmail.com'>Get Started</a>
+
+                 </button>
+
+            </div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default GetStarted
